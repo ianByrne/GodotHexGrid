8513c9e72207f53c033bdc4f062e7fe7cab3f271
Ian Byrne <ian.byrne@burnsie.com.au>
2021-11-21 23:29:45 +0000

    Move HexCell index calculation to struct

--- HexTiles/HexCell.cs
+++ HexTiles/HexCell.cs
@@ -14,7 +14,7 @@ namespace IanByrne.HexTiles
             
             CubeCoordinates = rounded;
             MovementCost = movementCost;
-            Index = index;
+            Index = CalculateIndex(rounded);
         }
 
         public HexCell(Vector2 axialCoordinates, int movementCost = 0) : this(axialCoordinates.ToCubeCoordinates(), movementCost) { }
@@ -22,11 +22,49 @@ namespace IanByrne.HexTiles
 
         public Vector3 CubeCoordinates { get; }
         public Vector2 AxialCoordinates => CubeCoordinates.ToAxialCoordinates();
-        public int Index { get; set; }
+        public int Index { get; }
 
         /// <summary>
         /// A value of -1 is impassable
         /// </summary>
         public int MovementCost { get; set; }
+
+        private static int CalculateIndex(Vector3 cubeCoordinates)
+        {
+            // Get the distance/radius from the origin (the MAX ABS of the cubeCoords)
+            // Can alternatively sum the ABS of all axes and halve. TODO: Which is faster?
+            var absoluteVector3 = cubeCoordinates.Abs();
+            var maxAxis = absoluteVector3.MaxAxis();
+
+            int distanceFromOrigin = (int)absoluteVector3[(int)maxAxis];
+
+            // Index of the origin is 0
+            if (distanceFromOrigin == 0)
+                return 0;
+
+            // Count the number of cells in the filled inner spiral
+            var spiralCells = HexGrid.GetCubeCoordinateSpiral(cubeCoordinates, distanceFromOrigin - 1);
+            int fill = spiralCells.Length;
+
+            // Count the remaining number of cells on the outer ring
+            // It will be something between 1 and distanceFromOrigin * 6
+            var neighbour = HexGrid.DIRECTIONS[4] * distanceFromOrigin;
+            int outerRing = 1;
+
+            for (int i = 0; i < 6; ++i)
+            {
+                for (int j = 0; j < distanceFromOrigin; ++j)
+                {
+                    if (neighbour == cubeCoordinates)
+                        return fill + outerRing - 1;
+
+                    ++outerRing;
+
+                    neighbour = HexGrid.GetNeighbourCubeCoordinate(neighbour, i);
+                }
+            }
+
+            return fill + outerRing - 1;
+        }
     }
 }
--- HexTiles/HexGrid.cs
+++ HexTiles/HexGrid.cs
@@ -6,29 +6,17 @@ namespace IanByrne.HexTiles
 {
     public class HexGrid : Reference
     {
+        public static readonly Vector3[] DIRECTIONS = new Vector3[]
+        {
+            new Vector3(0, 1, -1),
+            new Vector3(1, 0, -1),
+            new Vector3(1, -1, 0),
+            new Vector3(0, -1, 1),
+            new Vector3(-1, 0, 1),
+            new Vector3(-1, 1, 0)
+        };
+
         private static readonly Vector2 DEFAULT_SIZE = new Vector2(1, (float)Math.Sqrt(3) / 2);
-
-        private static readonly Vector3[] DIRECTIONS_FLAT = new Vector3[]
-        {
-            new Vector3(0, 1, -1),  // N
-            new Vector3(1, 0, -1),  // NE
-            new Vector3(1, -1, 0),  // SE
-            new Vector3(0, -1, 1),  // S
-            new Vector3(-1, 0, 1),  // SW
-            new Vector3(-1, 1, 0)   // NW
-        };
-
-        private static readonly Vector3[] DIRECTIONS_POINTY = new Vector3[]
-        {
-            new Vector3(1, 0, -1),  // NE
-            new Vector3(1, -1, 0),  // E
-            new Vector3(0, -1, 1),  // SE
-            new Vector3(-1, 0, 1),  // SW
-            new Vector3(-1, 1, 0),  // W
-            new Vector3(0, 1, -1)   // NW
-        };
-
-        private Vector3[] DIRECTIONS;
 
         private IDictionary<Vector2, HexCell> _cells; // AxialCoordinates => HexCell
         private Vector2 _scale;
@@ -81,27 +69,55 @@ namespace IanByrne.HexTiles
             }
         }
 
-        public void SetCells(IDictionary<Vector2, HexCell> cells)
+        public static Vector3 GetNeighbourCubeCoordinate(Vector3 from, int direction, int offset = 1)
         {
-            var newCells = new Dictionary<Vector2, HexCell>();
+            var directionVec3 = DIRECTIONS[direction] * offset;
 
-            foreach (var cell in cells)
+            return (from + directionVec3).RoundCubeCoordinates();
+        }
+
+        public static Vector3[] GetCubeCoordinateRing(Vector3 from, int radius)
+        {
+            var cubeCoordinates = new List<Vector3>();
+
+            var cubeCoordinate = GetNeighbourCubeCoordinate(from, 4, radius);
+
+            for (int i = 0; i < 6; ++i)
             {
-                var newCell = cell.Value;
-                CalculateIndex(ref newCell);
-                newCells.Add(newCell.AxialCoordinates, newCell);
+                for (int j = 0; j < radius; ++j)
+                {
+                    cubeCoordinates.Add(cubeCoordinate);
+
+                    cubeCoordinate = GetNeighbourCubeCoordinate(cubeCoordinate, i);
+                }
             }
 
-            _cells = newCells;
+            return cubeCoordinates.ToArray();
+        }
+
+        public static Vector3[] GetCubeCoordinateSpiral(Vector3 from, int radius)
+        {
+            var cubeCoordinates = new List<Vector3>();
+
+            cubeCoordinates.Add(from);
+
+            for (int i = 1; i <= radius; ++i)
+            {
+                cubeCoordinates.AddRange(GetCubeCoordinateRing(from, i));
+            }
+
+            return cubeCoordinates.ToArray();
+        }
+
+        public void SetCells(IDictionary<Vector2, HexCell> cells)
+        {
+            _cells = cells;
         }
 
         public void SetCell(HexCell cell)
         {
             if (_cells.ContainsKey(cell.AxialCoordinates))
-            {
-                CalculateIndex(ref cell);
                 _cells[cell.AxialCoordinates] = cell;
-            }
         }
 
         public HexCell? GetCell(Vector2 axialCoordinates)
@@ -122,13 +138,6 @@ namespace IanByrne.HexTiles
         public HexCell? GetCellAtPixel(Vector2 coordinates)
         {
             return GetCell(_inverseTransform.Multiply(coordinates).RoundAxialCoordinates());
-        }
-
-        public Vector3 GetNeighbourCubeCoordinate(Vector3 from, int direction, int offset = 1)
-        {
-            var directionVec3 = DIRECTIONS[direction] * offset;
-
-            return (from + directionVec3).RoundCubeCoordinates();
         }
 
         public HexCell? GetNeighbourCell(HexCell from, int direction, int offset = 1)
@@ -171,25 +180,6 @@ namespace IanByrne.HexTiles
                 Math.Abs(from.z - target.z)) / 2;
         }
 
-        public Vector3[] GetCubeCoordinateRing(Vector3 from, int radius)
-        {
-            var cubeCoordinates = new List<Vector3>();
-
-            var cubeCoordinate = GetNeighbourCubeCoordinate(from, 4, radius);
-
-            for (int i = 0; i < 6; ++i)
-            {
-                for (int j = 0; j < radius; ++j)
-                {
-                    cubeCoordinates.Add(cubeCoordinate);
-
-                    cubeCoordinate = GetNeighbourCubeCoordinate(cubeCoordinate, i);
-                }
-            }
-
-            return cubeCoordinates.ToArray();
-        }
-
         public HexCell[] GetCellRing(HexCell from, int radius)
         {
             var cells = new List<HexCell>();
@@ -205,20 +195,6 @@ namespace IanByrne.HexTiles
             }
 
             return cells.ToArray();
-        }
-
-        public Vector3[] GetCubeCoordinateSpiral(Vector3 from, int radius)
-        {
-            var cubeCoordinates = new List<Vector3>();
-
-            cubeCoordinates.Add(from);
-
-            for (int i = 1; i <= radius; ++i)
-            {
-                cubeCoordinates.AddRange(GetCubeCoordinateRing(from, i));
-            }
-
-            return cubeCoordinates.ToArray();
         }
 
         public HexCell[] GetCellSpiral(HexCell from, int radius)
@@ -262,8 +238,6 @@ namespace IanByrne.HexTiles
 
             if (_mode == HexMode.FLAT)
             {
-                DIRECTIONS = DIRECTIONS_FLAT;
-
                 _transform = new Transform2D(
                     new Vector2(size.x * 3 / 4, size.y / 2),
                     new Vector2(0, size.y),
@@ -271,8 +245,6 @@ namespace IanByrne.HexTiles
             }
             else
             {
-                DIRECTIONS = DIRECTIONS_POINTY;
-
                 _transform = new Transform2D(
                     new Vector2(size.y, 0),
                     new Vector2(size.y / 2, size.x * 3 / 4),
@@ -280,52 +252,6 @@ namespace IanByrne.HexTiles
             }
 
             _inverseTransform = _transform.AffineInverse();
-        }
-
-        private void CalculateIndex(ref HexCell cell)
-        {
-            // TODO: Put this into HexCell somehow...
-
-            // Get the distance/radius from the origin (the MAX ABS of the cubeCoords)
-            // Can alternatively sum the ABS of all axes and halve. TODO: Which is faster?
-            var absoluteVector3 = cell.CubeCoordinates.Abs();
-            var maxAxis = absoluteVector3.MaxAxis();
-
-            int distanceFromOrigin = (int)absoluteVector3[(int)maxAxis];
-
-            // Index of the origin is 0
-            if (distanceFromOrigin == 0)
-            {
-                cell.Index = 0;
-                return;
-            }
-
-            // Count the number of cells in the filled inner spiral
-            var spiralCells = GetCubeCoordinateSpiral(cell.CubeCoordinates, distanceFromOrigin - 1);
-            int fill = spiralCells.Length;
-
-            // Count the remaining number of cells on the outer ring
-            // It will be something between 1 and distanceFromOrigin * 6
-            var neighbour = DIRECTIONS[4] * distanceFromOrigin;
-            int outerRing = 1;
-
-            for (int i = 0; i < 6; ++i)
-            {
-                for (int j = 0; j < distanceFromOrigin; ++j)
-                {
-                    if (neighbour == cell.CubeCoordinates)
-                    {
-                        cell.Index = fill + outerRing - 1;
-                        return;
-                    }
-
-                    ++outerRing;
-
-                    neighbour = GetNeighbourCubeCoordinate(neighbour, i);
-                }
-            }
-
-            cell.Index = fill + outerRing - 1;
         }
     }
 }
